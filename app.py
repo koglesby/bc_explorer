c203d0a9d53a5d5da97ca0c0a7ef9972a6275b62
JIAFENG_CHEN <wynnchen@umich.edu>
2022-10-04 16:12:19 +0800

    Initial front-end development
    Display releases of a single label to the webpage

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from scraping import get_label_page
 import json
 
 app = Flask(__name__)
 CORS(app)
 
-def load_data(filename):
-	with open(filename, 'r') as f:
-		return json.load(f)
+# def load_data(filename):
+# 	with open(filename, 'r') as f:
+# 		return json.load(f)
 
-def load_dicts():
-	return load_data('images.json), load_data('quotes.json')
+# def load_dicts():
+# 	return load_data('images.json'), load_data('quotes.json')
 
-def clear_nulls():
-	images, quotes = load_dicts()
-	new_images = {}
-	new_quotes = {}
-	for author in images.keys():
-		if images[author]:
-			new_images[author] = images[author]
-			new_quotes[author] = quotes[author]
-	return new_images, new_quotes
+# def clear_nulls():
+# 	images, quotes = load_dicts()
+# 	new_images = {}
+# 	new_quotes = {}
+# 	for author in images.keys():
+# 		if images[author]:
+# 			new_images[author] = images[author]
+# 			new_quotes[author] = quotes[author]
+# 	return new_images, new_quotes
 
-def create_authors_list(authors):
-	step = 3
-	return [authors[i:i+step] for i in range(0, len(authors), step)]
+# def create_authors_list(authors):
+# 	step = 3
+# 	return [authors[i:i+step] for i in range(0, len(authors), step)]
 
 @app.route("/")
-def hello_world():
-	images, quotes = clear_nulls()
-	authors = list( quotes.keys() )
-	authors = create_authors_list(authors)
-	return jsonify(images=images, quotes=quotes, authors=authors)
+def get_label_releases():
+	return jsonify(get_label_page("SVBKVLT", album_num=20))
