576565310fab36b02817d0f2141c486ff690ec57
koglesby <kevinjoglesby@gmail.com>
2022-10-30 14:42:05 -0700

    removed unused variable

--- client/src/components/LabelsContainer.vue
+++ client/src/components/LabelsContainer.vue
@@ -1,34 +1,35 @@
 <template>
-    <div class="labels_container">
-      <div v-for="(label_url, label_name, index) in label_data">
-        <LabelReleases :label_name="label_name" :label_url="label_url"></LabelReleases>
-      </div>
+  <div class="labels_container">
+    <div v-for="(label_url, label_name) in label_data" v-bind:key="label_name">
+      <LabelReleases :label_name="label_name" :label_url="label_url"></LabelReleases>
     </div>
+  </div>
 </template>
 <script>
-  import LabelReleases from './LabelReleases.vue';
-  export default {
-    data() {
-      return {
-          label_data: {},
-      };
-    },
-    async created() {
-        const url = "http://127.0.0.1:5000/all_labels";
-        fetch(url, { credentials: "same-origin" })
-            .then((response) => {
-            if (!response.ok)
-                throw Error(response.statusText);
-            return response.json();
-        })
-            .then((data) => {
-            this.label_data = data;
-        })
-            .catch((error) => console.log(error));
-    },
-    components: { LabelReleases }
+import LabelReleases from './LabelReleases.vue';
+export default {
+  data() {
+    return {
+      label_data: {},
+    };
+  },
+  async created() {
+    const url = "http://127.0.0.1:5000/all_labels";
+    fetch(url, { credentials: "same-origin" })
+      .then((response) => {
+        if (!response.ok)
+          throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        this.label_data = data;
+      })
+      .catch((error) => console.log(error));
+  },
+  components: { LabelReleases }
 }
 </script>
 
 <style>
+
 </style>
